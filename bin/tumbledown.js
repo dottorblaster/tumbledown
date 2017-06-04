48bb8f83c49e3f721a92992c5d4466895536e32a
Alessio Biancalana <dottorblaster@gmail.com>
2017-06-04 15:04:27 +0200

    fixing tabs

--- bin/tumbledown.js
+++ bin/tumbledown.js
@@ -4,13 +4,13 @@ const cli = require('commander')
 var lib = require('../lib/index')
 
 cli
-    .command('login')
-    .description('Setup your tokens for use with Tumbleweed')
-    .action(lib.login)
+	.command('login')
+	.description('Setup your tokens for use with Tumbleweed')
+	.action(lib.login)
 
 cli
-    .command('post')
-    .description('Post a markdown file')
-    .action(lib.post)
+	.command('post')
+	.description('Post a markdown file')
+	.action(lib.post)
 
 cli.parse(process.argv)
