c31875cb6ab0be170feeb633238b8c71a6ac3692
Alessio Biancalana <dottorblaster@gmail.com>
2017-06-04 05:33:34 +0200

    actual first commit with code

---
+++ bin/tumbledown.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+const cli = require('commander')
+var lib = require('../lib/index')
+
+cli
+    .command('login')
+    .description('Setup your tokens for use with Tumbleweed')
+    .action(lib.login)
+
+cli
+    .command('post')
+    .description('Post a markdown file')
+    .action(lib.post)
